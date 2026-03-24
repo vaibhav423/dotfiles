4396e940cc770ef9e4e0c68fe60d5631a611e2ac
vaibhav423 <20vaibhav2007@gmail.com>
2026-03-24 07:30:19 +0530

    new

--- pc/zshrc/999-endscript.zsh
+++ pc/zshrc/999-endscript.zsh
@@ -10,7 +10,8 @@ export TERM=xterm-256color
 
 source ~/Water/crap/python/bin/activate
 
-
 export NVM_DIR="$HOME/.config/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/.secrets
