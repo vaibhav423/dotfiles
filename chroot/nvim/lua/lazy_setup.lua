8842df0c4bb6eafd9ad2d1946b3617cc041661a0
vaibhav423 <20vaibhav2007@gmail.com>
2026-04-14 01:06:55 +0530

    new

--- chroot/nvim/lua/lazy_setup.lua
+++ chroot/nvim/lua/lazy_setup.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
   },
   { import = "community" },
   { import = "plugins" },
+  {import = "config"}
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrotheme", "habamax" } },
