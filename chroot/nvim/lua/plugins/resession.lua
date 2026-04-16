73afce2bde8fea9dab2db4e698babe49277118f9
vaibhav <20vaibhav2007@gmail.com>
2026-04-16 18:11:55 +0000

    new

--- chroot/nvim/lua/plugins/resession.lua
+++ chroot/nvim/lua/plugins/resession.lua
@@ -1,6 +1,29 @@
 return {
   {
     "stevearc/resession.nvim",
+    init = function()
+      -- Inject the 'cwd' extension directly into memory so we don't need extra folders
+      package.loaded["resession.extensions.cwd"] = {
+        on_save = function(opts)
+          if opts and opts.tabpage then
+            local ok, num = pcall(vim.api.nvim_tabpage_get_number, opts.tabpage)
+            if ok then
+              return { cwd = vim.fn.getcwd(-1, num), is_tab = true }
+            end
+          end
+          return { cwd = vim.fn.getcwd(), is_tab = false }
+        end,
+        on_post_load = function(data)
+          if not data or not data.cwd then return end
+          
+          if data.is_tab then
+            pcall(vim.cmd, "tcd " .. vim.fn.fnameescape(data.cwd))
+          else
+            pcall(vim.api.nvim_set_current_dir, data.cwd)
+          end
+        end,
+      }
+    end,
     opts = function(_, opts)
       opts.extensions = opts.extensions or {}
       opts.extensions.cwd = { enable_in_tab = true }
