5a4d81ff7e43a8c063a75c29655852652111fdb9
vaibhav423 <20vaibhav2007@gmail.com>
2026-06-05 14:07:44 +0530

    new

--- chroot/scripts/captiv.sh
+++ chroot/scripts/captiv.sh
@@ -1,9 +1,15 @@
-sudo lsof -t -i :8080 | xargs -r sudo kill -9
-sudo pkill -u dns_tether dnsmasq
-sudo iptables -t nat -I PREROUTING -i wlan1 -p tcp --dport 53 -j REDIRECT --to-port 1053
-sudo iptables -t nat  -I PREROUTING -i wlan1 -p udp --dport 53 -j REDIRECT --to-port 1053
-sudo iptables -t nat -I PREROUTING -i wlan1 -p tcp --dport 80 -j REDIRECT --to-port 8080
-sudo dnsmasq --no-resolv --port=1053 --interface=wlan1 \
-  --address=/#/$(sudo ip -4 -o addr show dev wlan1 | awk '{print $4}' | cut -d/ -f1) \
-  --pid-file=/sdcard/dnsmasq-ap0.pid
+proxy_port=8080
+dnsmasq_user=dns_tether
+#interface=wlan1
+interface=wlan1
+dns_port=1053
+http_port=80
 
+sudo lsof -t -i :"$proxy_port" | xargs -r sudo kill -9
+sudo pkill -u "$dnsmasq_user" dnsmasq
+sudo iptables -t nat -I PREROUTING -i "$interface" -p tcp --dport 53 -j REDIRECT --to-port "$dns_port"
+sudo iptables -t nat  -I PREROUTING -i "$interface" -p udp --dport 53 -j REDIRECT --to-port "$dns_port"
+sudo iptables -t nat -I PREROUTING -i "$interface" -p tcp --dport "$http_port" -j REDIRECT --to-port "$proxy_port"
+sudo dnsmasq --no-resolv --port="$dns_port" --interface="$interface" \
+  --address=/#/$(sudo ip -4 -o addr show dev "$interface" | awk '{print $4}' | cut -d/ -f1) \
+  --pid-file=/sdcard/dnsmasq-ap0.pid
